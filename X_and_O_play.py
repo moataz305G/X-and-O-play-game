cf9d79b2479bc1892b2296e5086c205f9ae46963
moataz305G <moatazkh@gmail.com>
2025-06-27 12:30:33 +0300

    Revert "my edit in file_edit breansh"
    This reverts commit 000310e97b99c3ed38e2eb0f06f7f6150f41ac34.

--- X_and_O_play.py
+++ X_and_O_play.py
@@ -41,7 +41,7 @@ def check_winner(player):
     win_condition = [
         [0,1,2], [3,4,5], [6,7,8],  # صفوف
         [0,3,6], [1,4,7], [2,5,8],  # أعمدة
-        [0,4,8], [2,4,6]            
+        [0,4,8], [2,4,6]            # أقطار
     ]
     for condition in win_condition:
         if all(Control_panel[i] == player for i in condition):
